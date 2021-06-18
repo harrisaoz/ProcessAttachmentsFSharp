c409e34f1e2843ac5356307e959c81053c8a5a0a
Anthony Harris <anthony@ah.id.au>
2021-06-18 15:21:47 +0100

    Remove demo behaviour from real program.

--- SaveAttachments/Program.fs
+++ SaveAttachments/Program.fs
@@ -1,15 +1,8 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
-open System.ComponentModel
-
 module ET = ProcessAttachments.Execution.Templates
-
-open System
 
 open MailKit
 open MailKit.Net.Imap
 
-open ProcessAttachments.Collections
 open ProcessAttachments.DomainInterface
 open ProcessAttachments.ImapKit.ImapService
 open FSharp.Core.Extensions
@@ -21,13 +14,6 @@ module Message = ProcessAttachments.ImapKit.ImapMessage
 let personalNamespace =
     fun (client: ImapClient) ->
         client.PersonalNamespaces.Item(0)
-
-type FakeSession(id: int) =
-    member _.Open = id
-    
-    interface IDisposable with
-        member this.Dispose() =
-            ()
 
 [<EntryPoint>]
 let main argv =
@@ -60,7 +46,7 @@ let main argv =
         exportContent = fun folder message attachment ->
             eprintfn $"Export attachment: [{folder.FullName}] [{message.Envelope.Subject}] [{attachment.FileName}]"
             Ok 0L
-        onCompletion = fun (ok, failed) ->
+        onCompletion = fun (_, failed) ->
             List.length failed
         inspectNode = fun node ->
             eprintfn $"Inspecting folder: {node.FullName}"
@@ -71,52 +57,4 @@ let main argv =
         identifyLeaf = fun leaf -> $"[subject {leaf.Envelope.Subject}][date {leaf.Envelope.Date}]"
     }
 
-    let behaviourTest = {
-        defaultConfigFilename = "Config1.json"
-        configuration = fun _ -> () |> Ok
-        session = fun _ -> new FakeSession(17)
-        container = fun _ ->
-            ()|> Ok
-        roots = fun _ _ -> seq {0} |> Ok
-        nodes = fun root ->
-            let validate = Ok
-            let ls x =
-                match x with
-                | 0 -> seq {1;6;7}
-                | 1 -> seq {2;3;4}
-                | 4 -> seq {5}
-                | 7 -> seq {8;9}
-                | _ -> Seq.empty
-                |> Ok
-            Conversions.dfsPre validate ls root
-        closeNode = Result.map (fun n ->
-            eprintfn $"[{string n}] Closing node"
-            eprintfn "---------------------"
-            n)
-        leaves = fun x ->
-            match x with
-            | 1 -> seq {100; 101}
-            | 2 -> seq {200; 201; 202}
-            | 5 -> seq {500}
-            | 8 -> seq { 800 .. 804 }
-            | _ -> Seq.empty
-            |> Ok
-        contentItems = fun x ->
-            match x with
-            | small when small < 200 -> seq { small * 10 }
-            | big when big > 700 -> seq { big * 10 }
-            | _ -> Seq.empty
-        exportContent = fun n l c ->
-            eprintfn $"[{string n}] Export content [{string l}, {string c}]"
-            Ok (Convert.ToInt64 c)
-        onCompletion = fun (ok, failed) ->
-            Seq.length failed
-        inspectNode = fun n ->
-            eprintfn $"[{string n}] node"
-            n
-        inspectLeaf = fun l ->
-            eprintfn $"[{string l}] leaf"
-        identifyNode = string
-        identifyLeaf = string
-    }
-    argv |> ET.main behaviourTest
+    argv |> ET.main behaviour
