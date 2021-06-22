11a7bf68e1727962927f49dd92f7936b404ec0f5
Anthony Harris <anthony@ah.id.au>
2021-06-22 16:29:12 +0100

    Cleanup old commented out code.

--- ImapKit/ImapFolder.fs
+++ ImapKit/ImapFolder.fs
@@ -16,19 +16,6 @@ let listFoldersInNamespace (fNamespace: ImapClient -> FolderNamespace) (client: 
         .GetFolder(fNamespace client)
         .GetSubfolders(false)
 
-//let categorizeAttachment (behaviour: Behaviour) (mimePart: MimePart) =
-//    behaviour.accept mimePart
-//
-//let saveMessageAttachment (folder: IMailFolder) (summary: IMessageSummary) (attachment: BodyPartBasic) =
-//    match folder.GetBodyPart(summary.UniqueId, attachment) with
-//    | :? MimePart as mimePart ->
-//        match categorizeAttachment basicDownloader mimePart with
-//        | Warn msg -> Error msg
-//        | Ignore msg -> Ok msg
-//        | Accept ->
-//            Ok "todo"
-//    | _ -> Error $"Non-MIME attachment found [{attachment.ContentType.Name}]"
-
 let enumerateMessages searchQuery (folder: IMailFolder) =
     let messageFields =
         MessageSummaryItems.UniqueId
@@ -45,12 +32,6 @@ let enumerateMessages searchQuery (folder: IMailFolder) =
     let query = searchQuery |> Option.defaultValue SearchQuery.NotDeleted
     tryFetch query messageFields headers folder
     |> Result.map (fun l -> l :> IMessageSummary seq)
-            
-
-// This corresponds to AttachmentProcessor.VisitFolder
-// Fetch messages from a given folder
-// For each message, list attachments
-// For each attachment, save it
 
 let closeFolder elevatedFolder =
     elevatedFolder |> Result.bind tryCloseFolder
