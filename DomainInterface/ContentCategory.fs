140a64e3925b341fbefb9da484ddee681054be93
Anthony Harris <anthony@ah.id.au>
2021-06-23 02:15:40 +0100

    Re-order Content Category guards with success before failure.
    Also:
    - provide a bind-oriented implementation of the folder (currently as documentation only)

--- DomainInterface/ContentCategory.fs
+++ DomainInterface/ContentCategory.fs
@@ -4,30 +4,45 @@ type Reject = string
 type Ignore = string
 
 type ContentCategory<'a, 'Error> =
-    | Reject of 'Error
-    | Ignore
     | Accept of 'a
+    | Ignore
+    | Reject of 'Error
 
 module ContentCategory =
     let inline bind f c =
         match c with
-        | Reject r -> Reject r
-        | Ignore -> Ignore
         | Accept p -> f p
+        | Ignore -> Ignore
+        | Reject r -> Reject r
 
     let inline map f c = bind (fun p -> Accept (f p)) c
 
+//    let inline bind2 f v category =
+//        match category with
+//        | Accept x -> f x
+//        | Ignore -> v
+//        | Reject r -> Reject r
+//
+//    let folder acc category =
+//        let f x =
+//            let f' xs = Accept (Seq.singleton x |> Seq.append xs)
+//
+//            acc
+//            |> bind2 f' (Seq.singleton x |> Accept)
+//
+//        bind2 f acc category
+
     let folder (acc: ContentCategory<'a seq, 'b>) (c: ContentCategory<'a, 'b>) =
         match c with
-        | Reject r ->
-            Reject r
-        | Ignore ->
-            acc
         | Accept p ->
             match acc with
-            | Reject r0 ->
-                Reject r0
-            | Ignore ->
-                Accept (Seq.singleton p)
             | Accept ps ->
                 Accept (Seq.singleton p |> Seq.append ps)
+            | Ignore ->
+                Accept (Seq.singleton p)
+            | Reject r0 ->
+                Reject r0
+        | Ignore ->
+            acc
+        | Reject r ->
+            Reject r
